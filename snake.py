e7900808d41361fad619b9e8254a44aee829a7d2
codegenie-hackabot <codegenie-bot@proton.me>
2026-03-16 17:24:27 -0700

    Add simple snake game

--- snake.py
+++ snake.py
@@ -1,74 +1,41 @@
-#!/usr/bin/env python3
-"""
-A tiny, tongue‑in‑cheek Snake game.
-
-Run:
-    python3 snake.py
-
-The snake moves with the arrow keys. When it crashes into a wall or itself,
-a friendly hiss is printed and the game ends.
-"""
-
-import curses, random, sys, time
-
-WIDTH=40
-HEIGHT=20
-SNAKE_CHAR="🐍"
-FOOD_CHAR="🍎"
-DELAY=0.1
-
-def draw_border(win):
-    for x in range(WIDTH+2):
-        win.addch(0,x,"#")
-        win.addch(HEIGHT+1,x,"#")
-    for y in range(1,HEIGHT+1):
-        win.addch(y,0,"#")
-        win.addch(y,WIDTH+1,"#")
-
-def place_food(snake):
-    while True:
-        pos=(random.randint(1,HEIGHT),random.randint(1,WIDTH))
-        if pos not in snake:
-            return pos
+import random, curses, time
 
 def main(stdscr):
     curses.curs_set(0)
-    stdscr.nodelay(True)
-    stdscr.timeout(int(DELAY*1000))
-    snake=[(HEIGHT//2,WIDTH//2-1),(HEIGHT//2,WIDTH//2),(HEIGHT//2,WIDTH//2+1)]
-    direction=(0,1)
-    food=place_food(snake)
+    sh, sw = stdscr.getmaxyx()
+    w = curses.newwin(sh, sw, 0, 0)
+    w.keypad(1)
+    w.timeout(100)
+    snk_x = sw//4
+    snk_y = sh//2
+    snake = [[snk_y, snk_x], [snk_y, snk_x-1], [snk_y, snk_x-2]]
+    food = [random.randint(1, sh-2), random.randint(1, sw-2)]
+    w.addch(food[0], food[1], curses.ACS_PI)
+    key = curses.KEY_RIGHT
     while True:
-        try: key=stdscr.getkey()
-        except curses.error: key=None
-        if key in ("KEY_UP","w"): direction=(-1,0)
-        elif key in ("KEY_DOWN","s"): direction=(1,0)
-        elif key in ("KEY_LEFT","a"): direction=(0,-1)
-        elif key in ("KEY_RIGHT","d"): direction=(0,1)
-        elif key in ("q","Q"): break
-        hy,hx=snake[-1]
-        dy,dx=direction
-        nh=(hy+dy,hx+dx)
-        if nh[0]<1 or nh[0]>HEIGHT or nh[1]<1 or nh[1]>WIDTH or nh in snake:
-            stdscr.addstr(HEIGHT//2,WIDTH//2-5,"HISS! ☠️")
-            stdscr.refresh(); time.sleep(1.5); break
-        snake.append(nh)
-        if nh==food:
-            food=place_food(snake)
+        next_key = w.getch()
+        key = key if next_key == -1 else next_key
+        if key == curses.KEY_DOWN:
+            new_head = [snake[0][0]+1, snake[0][1]]
+        elif key == curses.KEY_UP:
+            new_head = [snake[0][0]-1, snake[0][1]]
+        elif key == curses.KEY_LEFT:
+            new_head = [snake[0][0], snake[0][1]-1]
         else:
-            snake.pop(0)
-        stdscr.clear()
-        draw_border(stdscr)
-        fy,fx=food
-        stdscr.addstr(fy,fx,FOOD_CHAR)
-        for y,x in snake[:-1]: stdscr.addstr(y,x,"·")
-        hy,hx=snake[-1]
-        stdscr.addstr(hy,hx,SNAKE_CHAR)
-        stdscr.refresh()
-    stdscr.nodelay(False)
-    stdscr.addstr(HEIGHT+3,0,"Game over. Press any key to exit...")
-    stdscr.getkey()
+            new_head = [snake[0][0], snake[0][1]+1]
+        snake.insert(0, new_head)
+        if snake[0][0] in [0, sh] or snake[0][1] in [0, sw] or snake[0] in snake[1:]:
+            break
+        if snake[0] == food:
+            food = None
+            while food is None:
+                nf = [random.randint(1, sh-2), random.randint(1, sw-2)]
+                if nf not in snake:
+                    food = nf
+            w.addch(food[0], food[1], curses.ACS_PI)
+        else:
+            tail = snake.pop()
+            w.addch(tail[0], tail[1], ' ')
+        w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
 
-if __name__=="__main__":
-    try: curses.wrapper(main)
-    except KeyboardInterrupt: sys.exit(0)
+curses.wrapper(main)
